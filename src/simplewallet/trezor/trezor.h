93330ce3abccb3010b82601de29386e2f50be6f4
Dion Ahmetaj <ahmetajd@coned.com>
2016-11-23 14:03:37 -0500

    trezor

--- src/simplewallet/trezor/trezor.h
+++ src/simplewallet/trezor/trezor.h
@@ -13,9 +13,10 @@ namespace simplewallet
       {
         private:
           
+          using protobuf_ptr = std::unique_ptr<protobuf::pb::Message>;
           std::unique_ptr<core::kernel> m_kernel;
           std::string m_session_id;
-          bool handle_call(const wire_codec::pbuf_type& pbuf, const wire::message& wire_in, wire::message& wire_out);          
+          bool handle_call(const protobuf_ptr& pbuf_in, protobuf_ptr& pbuf_out);          
           
         public:
           
